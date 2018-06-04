f75829c672c09eb256af9f2ec8adbb3578b0c1f9
xiatian002 <zhang_peng101@126.com>
2018-06-04 22:59:54 +0800

    创建Django项目

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#startup helloworld django project
+#nohup python3 manage.py runserver 0.0.0.0:8000 &
+file_date=`date +%Y-%m-%d`
+nohup python3 manage.py runserver 0.0.0.0:8000 >> ./logs/${file_date}.logs 2>&1 &
+process_id=`ps -ef|grep manage.py|grep -v grep|awk '{print $2}'`
+process_port=`netstat -anp|grep 8000|awk '{print $6}'`
+if [ ! -n "${process_id}" ]; then
+  echo "error start hello failed"
+elif [ ${process_port} == "LISTEN" ]; then
+  echo "hello is running"
+fi
+#echo "server start successfully"
